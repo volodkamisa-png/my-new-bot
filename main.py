03251d5204f1fbe14657a6d3385e866f5517d8d8
volodkamisa-png <volodkamisa@gmail.com>
2026-08-13 13:54:10 +0200

    Fix typo in TOKEN variable name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 
-TOKETTOKEN = os.environ.get("TOKEN")
+TOKEN = os.environ.get("TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Привіт! Напиши мені опис музики, яку хочеш створити.")
 
